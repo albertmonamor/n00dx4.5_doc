0bd24b1ce98830e401a7015083bfdf206cf9ebe7
Avraham sabann <66841899+albertmonamor@users.noreply.github.com>
2023-09-12 02:17:42 +0300

    Update NOODx4.5.py

--- NOODx4.5.py
+++ NOODx4.5.py
@@ -8,22 +8,31 @@ class NOODx4_5(TcpConnect):
 	
     def __init__(self):
 		pass
+		
     def TCP_RUN(self):
 		pass
+		
     def TCP_COMMUNICATION(self):
 		pass
+		
     def mode_ping(self):
 		pass
+		
     def mode_shell(self, _info):
 		pass
+		
     def mode_screen(self, _info):
 		pass
+		
     def mode_explorer(self, _info):
 		pass
+		
     def mode_streamer(self, _info: dict):
 		pass
+		
     def mode_scanner(self, _info:dict):
 		pass
+		
     def ForceClean(self):
 		pass
         
