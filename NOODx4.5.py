99965a8ddca04a32843a24db45b0a4bda4c1c7ef
Avraham sabann <66841899+albertmonamor@users.noreply.github.com>
2023-09-12 01:47:58 +0300

    Create NOODx4.5.py

---
+++ NOODx4.5.py
@@ -0,0 +1,29 @@
+from Api.scanner_api import set_sig
+from Api.tcp_connect import *
+from Api.mode_api import *
+from threading import Thread
+
+
+class NOODx4_5(TcpConnect):
+	
+    def __init__(self):
+		pass
+    def TCP_RUN(self):
+		pass
+    def TCP_COMMUNICATION(self):
+		pass
+    def mode_ping(self):
+		pass
+    def mode_shell(self, _info):
+		pass
+    def mode_screen(self, _info):
+		pass
+    def mode_explorer(self, _info):
+		pass
+    def mode_streamer(self, _info: dict):
+		pass
+    def mode_scanner(self, _info:dict):
+		pass
+    def ForceClean(self):
+		pass
+        
